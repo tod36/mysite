c6ff08136a40584b91672571d3ad6f379deb2a97
Todor Mitrov <todor36@gmail.com>
2021-09-27 18:42:22 +0300

    Initial commit

---
+++ news/forms.py
@@ -0,0 +1,24 @@
+import re
+
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import News
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match('\d', title):
+            raise ValidationError('Название  не должно начинаеться с цифром')
+        return title
